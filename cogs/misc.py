f3a01a7291331a65265bc4c29691cf4b216e55d2
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-05-01 15:57:28 +0530

    added info group, contains server and user commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,6 +3,13 @@ from discord.ext import commands
 from discord.ext.commands import Context
 from discord import app_commands
 
+default_color = discord.Color.from_rgb(241, 227, 226)
+
+def format_date_with_suffix(date):
+	day = date.day
+	suffix = "th" if 11 <= day <= 13 else {1: "st", 2: "nd", 3: "rd"}.get(day % 10, "th")
+	return date.strftime(f"{day}{suffix} %b, %Y")
+
 class misc(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
@@ -11,23 +18,51 @@ class misc(commands.Cog):
 	async def on_ready(self):
 		print(f"{__name__} is online!")
 	
-	# serverinfo
-	@commands.hybrid_command(name="serverinfo", aliases=["svinfo"], help="info about the server")
-	async def serverinfo(self, ctx):
-		embed = discord.Embed(
-			title=ctx.guild,
-			color=discord.Color.from_rgb(241, 227, 226)
-			)
-		if ctx.guild.icon is not None:
-			embed.set_thumbnail(url=ctx.guild.icon.url)
-			# embed.set_author(icon_url=ctx.guild.icon.url)
-		# embed.add_field(name="Server ID", value=ctx.guild.id)
-		embed.add_field(name="Members", value=ctx.guild.member_count)
-		embed.add_field(name="Channels", value=f"{len(ctx.guild.channels)}")
-		embed.add_field(name=f"Roles", value=f"{len(ctx.guild.roles)}")
-		# embed.set_footer(text=f"Created at: {ctx.guild.created_at}")
+	# info (group)
+	@commands.hybrid_group(help="tools for info and stats")
+	async def info(eslf, ctx):
+		if ctx.invoked_subcommand is None:
+			pass
+
+	# info server
+	@info.command(name="server", help="info on current server")
+	async def server(self, ctx):
+		try:
+			guild = ctx.guild
+			embed = discord.Embed(
+				title=guild.name,
+				color=default_color
+				)
+			if guild.icon:
+				embed.set_thumbnail(url=guild.icon.url)
+
+			embed.add_field(name="members", value=guild.member_count)
+			embed.add_field(name="channels", value=f"{len(guild.channels)}")
+			embed.add_field(name=f"roles", value=f"{len(guild.roles)}")
+			embed.set_footer(text=f"created at: {guild.created_at}") # crowded format to show info, fix this
+			
+			await ctx.send(embed=embed)
 		
-		await ctx.send(embed=embed)
+		except Exception as e:
+			await ctx.send(f"error: {e}")
+
+	# info user
+	@info.command(name="user", help="info on specified user")
+	async def user(self, ctx, user: discord.User = None):
+		try:
+			user = user or ctx.author # default to command initiator if not user given
+			embed = discord.Embed(
+				title=f"{user.name}#{user.discriminator}",
+				color=default_color,
+			)
+
+			embed.set_thumbnail(url=user.avatar.url if user.avatar else user.default_avatar.url)
+			embed.add_field(name="created at", value=f"{format_date_with_suffix(user.created_at)}\n-# {user.created_at.strftime('%H: %M: %S')}")
+
+			await ctx.send(embed=embed)
+
+		except Exception as e:
+			await ctx.send(f"error: {e}")
 
 async def setup(bot):
 	await bot.add_cog(misc(bot))
