31135a3d92ccf8a21e785e22cb78989073f1f5f9
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-05-18 19:17:16 +0530

    renamed + added ,dvc as a list dynamicvcs list

---
+++ cogs/universium.py
@@ -0,0 +1,101 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from discord import app_commands
+from utils.json_handler import json_load, json_save
+
+class universium(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+	
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"{__name__} is online!")
+	
+	################################################
+	################################################
+
+	# listener for dynamic vc
+	@commands.Cog.listener()
+	async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+		if before.channel == after.channel:return
+
+		data = json_load("dynamicvc")
+		enabled_vcs = data.setdefault("enabled_vcs", {})
+
+		# gather unique vcs involved in event
+		affected_channels = {before.channel, after.channel}
+
+		for vc in affected_channels:
+			if not vc or str(vc.id) not in enabled_vcs:
+				continue
+
+		guild = vc.guild
+		everyone = guild.default_role
+		current_overwrite = vc.overwrites_for(everyone)
+		member_count = len(vc.members)
+
+		# Toggle visibility based on member count
+		if member_count == 0 and current_overwrite.view_channel is not False:
+			# no one left in vc
+			current_overwrite.view_channel = False
+			await vc.set_permissions(everyone, overwrite=current_overwrite)
+
+		elif member_count > 0 and current_overwrite.view_channel != True:
+			# someone joined vc
+			current_overwrite.view_channel = True
+			await vc.set_permissions(everyone, overwrite=current_overwrite)
+
+	# dynamicvc
+	@commands.hybrid_command(help="toggle dynamic vc", aliases="dvc")
+	@commands.has_permissions(administrator=True)
+	async def dynamicvc(self, ctx, channel: discord.VoiceChannel = None):
+		data = json_load("dynamicvc")
+		enabled = data.setdefault("enabled_vcs", {})
+
+		if channel is None:
+			channel = ctx.author.voice.channel if ctx.author.voice else None
+			if channel is None:
+				# await ctx.send("either specify a voice channel or be in one")
+
+				guild_vcs = []
+
+				for ch in enabled:
+					vc = ctx.guild.get_channel(int(ch))
+					if vc:
+						guild_vcs.append(f"- {vc.mention}")
+
+				if guild_vcs:
+					await ctx.send(">>> dynamic vcs:\n" + "\n".join(guild_vcs))
+				else:
+					await ctx.send(">>> no dynamic vcs")
+
+				return
+
+		channel_id = str(channel.id)
+
+		if channel_id in enabled:
+			del enabled[channel_id]
+			await channel.set_permissions(ctx.guild.default_role, overwrite=None)
+			await ctx.send(f"disabled dynamicvc for {channel.mention}")
+		else:
+			enabled[channel_id] = True
+			overwrite = channel.overwrites_for(ctx.guild.default_role)
+			if len(channel.members) == 0:
+				# hide channel if empty
+				overwrite.view_channel = False
+			else:
+				# show channel if not empty
+				overwrite.view_channel = True
+			
+			await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+
+			await ctx.send(f"enabled dynamicvc for {channel.mention}")
+		
+		json_save("dynamicvc", data)
+
+	################################################
+	################################################
+
+async def setup(bot):
+	await bot.add_cog(universium(bot))
