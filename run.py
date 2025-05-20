d4d7ccfb191903bf17a843c6bdd6cc05d1f44b1d
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-05-20 21:52:43 +0530

    added >>> to errors

--- run.py
+++ run.py
@@ -16,26 +16,26 @@ async def on_ready():
 @bot.event
 async def on_command_error(ctx, error):
 	if isinstance(error, commands.MissingPermissions):
-		await ctx.send("your aah lacks perms to use this cmd")
+		await ctx.send(">>> your aah lacks perms to use this cmd")
 	elif isinstance(error, commands.MissingRequiredArgument):
 		await ctx.send(">>> missing a required arg\n-# deleting..", delete_after=3)
 	elif isinstance(error, commands.BotMissingPermissions):
-		await ctx.send("i lack perms for ts :wilted:")
+		await ctx.send(">>> i lack perms for ts :wilted:")
 	elif isinstance(error, FileNotFoundError):
-		await ctx.send("contact yasu, json file not found")
+		await ctx.send(">>> contact yasu, json file not found")
 	elif isinstance(error, json.JSONDecodeError):
-		await ctx.send("contact yasu, error reading json file")
+		await ctx.send(">>> contact yasu, error reading json file")
 	elif isinstance(error, discord.Forbidden):
-		await ctx.send("contact yasu, bot lacks perms")
+		await ctx.send(">>> contact yasu, bot lacks perms")
 	elif isinstance(error, commands.CommandNotFound):
 		return
 	elif str(error).startswith("Role") and str(error).endswith("not found."):
 		await ctx.send(f">>> role doesn't exist")
 	else:
 		if str(error).startswith("The check functions for command"):
-			await ctx.send("command not allowed for this aah server\n-# deleting..", delete_after=3)
+			await ctx.send(">>> command not allowed for this aah server\n-# deleting..", delete_after=3)
 		else:
-			await ctx.send(f":warning: error :warning: contact yasu ```py\n{error}```")
+			await ctx.send(f">>> :warning: error :warning: contact yasu ```py\n{error}```")
 
 async def main():
 	async with bot:
