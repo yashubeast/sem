8ca083be5288cbd31c3ce64360f85b7d8b835551
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-05-29 21:54:59 +0530

    checkserver function for uni only cmds

--- utils/values.py
+++ utils/values.py
@@ -5,8 +5,9 @@ load_dotenv()
 
 default_color = discord.Color.from_rgb(241, 227, 226)
 
-def checkuni(ctx):
-	return ctx.guild.id == 1222206531479666740
+ALLOWED_SERVERS = list(map(int, os.getenv("ALLOWED_SERVERS", "").split(",")))
+def checkserver(ctx):
+	return ctx.guild.id in ALLOWED_SERVERS
 
 ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
 def checkadmin(ctx):
