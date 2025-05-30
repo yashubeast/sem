6c9c675fd62dba8269a9037058f96b14300dc446
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-05-30 19:53:29 +0530

    minor 2

--- gitpull.bat
+++ gitpull.bat
@@ -14,8 +14,8 @@ for /f "delims=" %%i in ('git rev-parse HEAD') do set AFTER=%%i
 
 REM show log only if updated
 if "%BEFORE%"=="%AFTER%" (
-    echo ==== New Commits Pulled ====
-    git log --oneline %BEFORE%..%AFTER%
-) else(
 	echo nothing
+) else(
+	echo ==== New Commits Pulled ====
+	git log --oneline %BEFORE%..%AFTER%
 )
