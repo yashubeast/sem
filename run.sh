bea891cea5a892318d2048f935f3ea7a2191ac24
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-06-04 00:46:42 +0530

    test: quiet pip install

--- run.sh
+++ run.sh
@@ -8,17 +8,7 @@ git pull origin main
 
 # install dependencies
 echo "Installing dependencies.."
-python -m pip install -r req.txt
-
-# function for token
-if [ ! -f .env ] || ! grep -q "^TOKEN=" .env; then
-
-	# prompt for token
-	read -p "Enter your bot token: " TOKEN
-
-	# save to .env
-	echo "TOKEN=$TOKEN" > .env
-fi
+python -m pip install -r req.txt -q
 
 # run the bot
 echo "Waking up sem.."
