c023958fc0ceafd49cd65e04100415958ef267bc
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-06-03 23:46:14 +0530

    test: docker

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# exit if any command fails
+set -e
+
+# pull latest changes from the repository
+git pull origin main
+
+# install dependencies
+echo "Installing dependencies.."
+python -m pip install -r req.txt
+
+# run the bot
+echo "Waking up sem.."
+python run.py
