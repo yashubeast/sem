ca906449bc9df8935ba3a154a4344b82a29ce5e2
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-06-04 00:40:56 +0530

    test: token prompt

--- run.sh
+++ run.sh
@@ -10,6 +10,16 @@ git pull origin main
 echo "Installing dependencies.."
 python -m pip install -r req.txt
 
+# function for token
+if [ ! -f .env ] || ! grep -q "^TOKEN=" .env; then
+
+	# prompt for token
+	read -p "Enter your bot token: " TOKEN
+
+	# save to .env
+	echo "TOKEN=$TOKEN" > .env
+fi
+
 # run the bot
 echo "Waking up sem.."
 python main.py
