c79800ed814d003805279fcdae253729414f0b6e
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-06-03 22:27:12 +0530

    fix: read env

--- utils/values.py
+++ utils/values.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 default_color = discord.Color.from_rgb(241, 227, 226)
 
-ALLOWED_SERVERS = list(map(int, os.getenv("ALLOWED_SERVERS", "").split(",")))
+ALLOWED_SERVERS = [int(x) for x in os.getenv("ALLOWED_SERVERS", "").split(",") if x.strip()]
 def checkserver(ctx):
 	return ctx.guild.id in ALLOWED_SERVERS
 
-ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
+ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x.strip()]
 def checkadmin(ctx):
 	return ctx.author.id in ADMINS
