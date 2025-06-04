1649e3013babdaaa3da99a85e94489b0021b622b
yashubeast <95849633+yashubeast@users.noreply.github.com>
2025-06-04 13:13:52 +0530

    feat: semity

---
+++ cogs/misc/semity.py
@@ -0,0 +1,117 @@
+import discord, aiohttp, time
+from discord.ext import commands
+from utils.values import checkserver
+
+class semity(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.user_last_message_time = {}
+	
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f"{__name__} is online!")
+
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		
+		if not checkserver(message): return
+
+		if message.author.bot:return
+
+		user_id = message.author.id
+		server_id = message.guild.id if message.guild else None
+		msg_len = len(message.content)
+
+		# time since last message
+		current_time = time.time()
+
+		if user_id in self.user_last_message_time:
+			time_since_last = current_time - self.user_last_message_time[user_id]
+		else:
+			time_since_last = None # first message from user
+
+		self.user_last_message_time[user_id] = current_time
+
+		if time_since_last is None: time_since_last = 0
+		
+		# message formula
+		async with aiohttp.ClientSession() as session:
+			async with session.post(
+				"http://localhost:8000/equity/message/formula",
+				json={
+					"user_id": user_id,
+					"server_id": server_id,
+					"message_length": msg_len,
+					"message_time_gap": time_since_last
+				}
+			) as resp:
+				if resp.status == 200:
+					data = await resp.json()
+					print(f'{message.author}: {message.guild.name} -> len: {data["message_length"]}, time: {data["message_time_gap"]}, msgs: {data["message_count"]} = {data["total"]}')
+				else:
+					print(f"api error: {resp.status}")
+
+		# message count
+		async with aiohttp.ClientSession() as session:
+			async with session.post(
+				"http://localhost:8000/equity/message/add",
+				json={
+					"user_id": user_id,
+					"server_id": server_id
+				}
+			) as resp:
+				if resp.status == 200:
+					data = await resp.json()
+					print(f"message count updated: {data['message_count']}")
+				else:
+					print(f"api error: {resp.status}")
+	
+	@commands.Cog.listener()
+	async def on_message_delete(self, message):
+
+		if message.author.bot or not message.guild:return
+
+		user_id = message.author.id
+		server_id = message.guild.id
+
+		async with aiohttp.ClientSession() as session:
+			url = "http://localhost:8000/messages/remove"
+			json = {
+				"user_id": user_id,
+				"server_id": server_id
+			}
+
+			async with session.post(url, json=json) as resp:
+				if resp.status == 404:
+					await message.channel.send(f"> no entry for user {message.author.mention}")
+				elif resp.status == 400:
+					await message.channel.send(f"> count 0 for user {message.author.mention}")
+				elif resp.status == 200:
+					data = await resp.json()
+					await message.channel.send(f"> message count updated: {data['message_count']} for {message.author.mention}")
+				else:
+					await message.channel.send(f"> error updating message count for {message.author.mention}")
+	
+	@commands.command(name="msgcount", description="get message count for a user, total/specified server")
+	async def msgcount(self, ctx, user: discord.User = None, server: int = None):
+		user_id = (user or ctx.author).id
+		server_name = await self.bot.fetch_guild(server) if server else None
+
+		async with aiohttp.ClientSession() as session:
+			url = f"http://localhost:8000/equity/message/{user_id}" + (f"?server_id={server}" if server else "")
+			async with session.get(url) as resp:
+				if resp.status == 404:
+					msg = ("> no entry for user")
+					if server:
+						msg += f" in specified server"
+					await ctx.send(msg)
+					return
+				
+				data = await resp.json()
+				msg = f"> {user.mention if user else ctx.author.mention} has sent {data['message_count']} messages"
+				if server_name:
+					msg += f" in {server_name}"
+				await ctx.send(msg)
+
+async def setup(bot):
+	await bot.add_cog(semity(bot))
